0ba7ff672bd308fa4072505bc110136bedf806f9
yimeng <513316336@qq.com>
2018-07-19 22:52:23 +0800

    init

---
+++ src/main/java/priv/yi/springboot/maven/demo/SpringBootMavenDemoApplication.java
@@ -0,0 +1,12 @@
+package priv.yi.springboot.maven.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenDemoApplication.class, args);
+	}
+}
